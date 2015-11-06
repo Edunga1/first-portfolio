c917d8525d8f40acf8b2a3175499170ce189cc2e
Edunga1 <gkfkr21@naver.com>
2015-11-07 03:21:04 +0900

    angularjs + @로 구조 변경 - css framework - skeleton, meryerweb-reset 추가 - jquery, bootstrap 제거

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 'use strict';
 
-
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: GenericViewCtrl});
-    $routeProvider.otherwise({redirectTo: '/home'});
-  }]);
+angular.module('App', ['ngRoute','App.filters', 'App.services', 'App.directives']).
+	config(
+		function($routeProvider) {
+			$routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: GenericViewCtrl});
+			$routeProvider.otherwise({redirectTo: '/home'});
+		}
+	);
--- js/services.js
+++ js/services.js
@@ -4,12 +4,4 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-var services = angular.module('myApp.services', []);
-
-services.value('version', '0.2');
-
-
-
-
-
-
+var services = angular.module('App.services', []);
