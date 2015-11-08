072cb9800b8e600ca28498f67dea0c4be2f3a860
Edunga1 <gkfkr21@naver.com>
2015-11-09 06:48:10 +0900

    디자인 수정 네비게이션 도킹 컨텐츠 추가

--- js/directives.js
+++ js/directives.js
@@ -4,8 +4,31 @@
 
 
 angular.module('App.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+	directive('navbar', function($window){
+		return {
+			restrict: 'A',
+			link: function(scope, element, attrs){
+				var position = element[0].style.position;
+				var offsetTop;
+				element.css('z-index', 999);
+				element.css('top', 0);
+				element.css('left', 0);
+				$window.addEventListener('scroll', function(){
+					if(element.css('position') != 'fixed'){
+						var elem = element[0];
+						offsetTop = element[0].offsetTop;
+						while(elem = elem.offsetParent){
+							offsetTop += elem.offsetTop;
+						}
+					}
+					if(this.pageYOffset > offsetTop){
+						element[0].style.position = 'fixed';
+						element[0].classList.add('docked');
+					}else{
+						element[0].style.position = position;
+						element[0].classList.remove('docked');
+					}
+				});
+			}
+		}
+	});
