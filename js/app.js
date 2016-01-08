6e36f1be5fce01ee1670a09a96ced6fb5a42d086
Edunga1 <gkfkr21@naver.com>
2016-01-08 22:47:02 +0900

    update

--- js/app.js
+++ js/app.js
@@ -5,9 +5,11 @@ angular.module('App', ['ngRoute','App.filters', 'App.services', 'App.directives'
 	config(
 		function($routeProvider) {
 			$routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: GenericViewCtrl});
+			$routeProvider.when('/test', {templateUrl: 'partials/test.html', controller: GenericViewCtrl});
+			$routeProvider.when('/reference', {templateUrl: 'partials/reference.html', controller: GenericViewCtrl});
 			$routeProvider.when('/kpool', {templateUrl: 'partials/kpool.html', controller: GenericViewCtrl});
 			$routeProvider.when('/travelmaker', {templateUrl: 'partials/travelmaker.html', controller: GenericViewCtrl});
-			$routeProvider.when('/reference', {templateUrl: 'partials/reference.html', controller: GenericViewCtrl});
+			$routeProvider.when('/cctv', {templateUrl: 'partials/cctv.html', controller: GenericViewCtrl});
 			$routeProvider.otherwise({redirectTo: '/home'});
 		}
 	);
